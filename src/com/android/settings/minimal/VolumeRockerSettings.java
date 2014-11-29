f717de1abd09484e0f8a8b8f431776701530ed15
Muhammed Nazim <mnm9994u@gmail.com>
2014-11-29 19:59:34 +0100

    Forward Port: Volume button seek on screen off [2/2] Adapted for Marshmallow by zephiK
    Change-Id: I788fc4cfbb688d99de241991821ebf7e277d19ad
    
     Conflicts:
    	res/values/minimal_strings.xml
    	src/com/android/settings/minimal/VolumeRockerSettings.java
    
     Author:    Muhammed Nazim <mnm9994u@gmail.com>
     Date:      Sat Nov 29 19:59:34 2014 +0100
     Changes to be committed:
    	modified:   res/values/minimal_strings.xml
    	modified:   res/xml/minimal_settings_volume.xml
    	modified:   src/com/android/settings/minimal/VolumeRockerSettings.java

--- src/com/android/settings/minimal/VolumeRockerSettings.java
+++ src/com/android/settings/minimal/VolumeRockerSettings.java
@@ -15,19 +15,45 @@ import com.android.settings.SettingsPreferenceFragment;
 public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-		// private variables here
-		
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+    private static final String KEY_VOLBTN_MUSIC_CTRL = "volbtn_music_controls";
+
+    private SwitchPreference mVolumeRockerWake;
+    private SwitchPreference mVolBtnMusicCtrl;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.minimal_settings_volume);
+
+        // volume rocker wake
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
+
+        // volume music control
+        mVolBtnMusicCtrl = (SwitchPreference) findPreference(KEY_VOLBTN_MUSIC_CTRL);
+        mVolBtnMusicCtrl.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_MUSIC_CONTROLS, 1) != 0);
+        mVolBtnMusicCtrl.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
-		// preference changes here
+        if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolBtnMusicCtrl) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_MUSIC_CONTROLS,
+                    (Boolean) objValue ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
