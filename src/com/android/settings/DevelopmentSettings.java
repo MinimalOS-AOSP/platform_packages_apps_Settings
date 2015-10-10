65b9e1260ebb230d5cd0e1c5289c0c6cb6119ec6
George G <kreach3r@users.noreply.github.com>
2015-10-10 18:25:33 +0300

    Enable Development Settings for everyone, too!
    User and userdebug builds were jealous.

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1906,7 +1906,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                     return context.getSharedPreferences(DevelopmentSettings.PREF_FILE,
                             Context.MODE_PRIVATE).getBoolean(
                                     DevelopmentSettings.PREF_SHOW,
-                                    android.os.Build.TYPE.equals("eng"));
+                                    true);
                 }
 
                 @Override
