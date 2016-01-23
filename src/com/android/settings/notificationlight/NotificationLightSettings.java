79920e95439266b7e9002d403b9827d91444952b
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2016-01-23 12:30:30 +0100

    Update metrics
    Signed-off-by: Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>

--- src/com/android/settings/notificationlight/NotificationLightSettings.java
+++ src/com/android/settings/notificationlight/NotificationLightSettings.java
@@ -94,7 +94,7 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.APPLICATION;
+        return MetricsLogger.NOTIFICATION_LIGHT_SETTINGS;
     }
 
     @Override
